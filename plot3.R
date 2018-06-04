aaadb5db68eadd8110b4c50d90be435db70134fa
Hao Xu <haoxu@vt.edu>
2018-06-04 01:08:35 -0400

    Update

--- plot3.R
+++ plot3.R
@@ -12,7 +12,7 @@ with(df1, {
         lines(Datetime, Sub_metering_3, col="blue")
 })
 legend("topright", col=c("black","red","blue"), 
-       lty=1, lwd=2,
+       lty=1,
        legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
 dev.copy(png, file="plot3.png",width=480, height=480)
